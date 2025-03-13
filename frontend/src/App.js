14fbc0e09e2bce316f56bbf91eb20fb793024fd5
JKRakuba <jullietrakuba@gmail.com>
2025-03-13 16:03:41 +0200

    Added background image for the HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Register from "./components/Register";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="register" element={<Register />} />
-    </Routes>
+<div>
+      <Navbar /> {<Navbar/>}
+     
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="register" element={<Register />} />
+        {/* You can add more routes for About, Login, etc. */}
+      </Routes>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,45 @@
+// src/components/Navbar.js
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
+import "../Css/Navbar.css"; // Import the CSS file for styling (optional)
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle mobile menu
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <Link to="/">CC Food Link</Link>
+        </div>
+
+        {/* Menu icon for mobile view */}
+        <div className="menu-icon" onClick={handleMenuToggle}>
+          <i className={isMenuOpen ? "fa fa-times" : "fa fa-bars"}></i>
+        </div>
+
+        {/* Navbar menu */}
+        <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

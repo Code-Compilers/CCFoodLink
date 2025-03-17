f3da1d202d09891459e7112dafe8524f43e7eca5
Bokang Khumalo <143175664+bokangkhumalo@users.noreply.github.com>
2025-03-17 02:16:06 +0000

    Create Donation confirmation page and css for Donors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Register from "./components/Register";
+import DonationConfirmation from "./components/DonationConfirmation";
 
 function App() {
   console.log('App component rendered');
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="register" element={<Register />} />
+      <Route path="donation-confirmation" element={<DonationConfirmation />} />
     </Routes>
   );
 }

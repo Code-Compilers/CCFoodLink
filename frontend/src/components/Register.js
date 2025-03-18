f434b0d64261697bdec23f83d7fc74e39da481e7
Bokang Khumalo <143175664+bokangkhumalo@users.noreply.github.com>
2025-03-18 12:15:38 +0000

    prepare files to compile

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'; 
+import { Link, useNavigate } from 'react-router-dom'; // Add this import statement
 import "../Css/Register.css"; // Import the updated CSS file for styling
 
 const Register = () => {
